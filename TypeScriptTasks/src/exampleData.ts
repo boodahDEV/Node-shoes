99e2fa5c6ecaa7b6be54023240665d39e77c893c
boodah <boodah21@protonmail.com>
2020-03-11 12:16:25 -0500

    Se ajusta el index a las salidas de las tareas como prueba.

---
+++ TypeScriptTasks/src/exampleData.ts
@@ -0,0 +1,8 @@
+import { TaskItem } from "./model/TaskItem";
+
+export const task: TaskItem[]=[
+    new TaskItem(1,"Task One"),
+    new TaskItem(2,"Task two"),
+    new TaskItem(3,"Task three"),
+    new TaskItem(4,"Task four", true)
+]
--- TypeScriptTasks/src/index.ts
+++ TypeScriptTasks/src/index.ts
@@ -1,1 +1,8 @@
-console.log("Hello world")
+import { Inquirer } from "inquirer";
+import { TaskCollection } from "./model/TaskCollection";
+import { task } from "./exampleData";
+
+const collection = new TaskCollection('fazt', task)
+collection.getTaskItems(false).forEach(item=>{
+    item.printDetails()
+})
