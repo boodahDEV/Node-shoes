383a0feb00dec0bc9190c25e6194ce00fea8bb69
boodah <boodah21@protonmail.com>
2020-09-02 00:34:02 -0500

    Mini CRUD firebase functions

--- API-REST-FIREBASE-EXAMPLE/functions/index.js
+++ API-REST-FIREBASE-EXAMPLE/functions/index.js
@@ -1,9 +1,83 @@
-const functions = require('firebase-functions');
+const functions = require("firebase-functions");
+const express = require("express");
+const cors = require("cors");
+const admin = require("firebase-admin");
+const { firestore } = require("firebase-admin");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const app = express();
+admin.initializeApp({
+  credential: admin.credential.cert("../firebase.json"),
+  databaseURL: "https://tester-proyect.firebaseio.com",
+});
+const db = admin.firestore();
+
+app.get("/hello-world", (req, res) => {
+  return res.status(200).json({ messaje: "hello world" });
+});
+
+app.post("/api/products", async (req, res) => {
+  try {
+    await db
+      .collection("products")
+      .doc("/" + req.body.id + "/")
+      .set({
+        name: req.body.name,
+      });
+    return res.status(204).json();
+  } catch (error) {
+    return res.status(500).send(error);
+  }
+});
+
+app.get("/api/products/:product_id", (req, res) => {
+  (async () => {
+    try {
+      const doc = db.collection("products").doc(req.params.product_id);
+      const items = await doc.get();
+      const response = items.data();
+      return res.status(200).json(response);
+    } catch (err) {
+      return res.status(500).send(error);
+    }
+  })();
+});
+
+app.get("/api/products", async (req, res) => {
+  try {
+    const query = db.collection("products");
+    const querySnapshot = await query.get();
+
+    const response = querySnapshot.docs.map((doc) => ({
+      id: doc.id,
+      name: doc.data().name,
+    }));
+
+    return res.status(200).json(response);
+  } catch (error) {
+    return res.status(500).send(error);
+  }
+});
+
+app.delete("/api/products/:product_id", async (req, res) => {
+  try {
+    const document = db.collection("products").doc(req.params.product_id);
+    await document.delete();
+    return res.status(204).json();
+  } catch (error) {
+    return res.status(500).send(error);
+  }
+});
+
+app.put("/api/products/:product_id", async (req, res) => {
+  try {
+    const document = db.collection("products").doc(req.params.product_id);
+    await document.update({
+      name: req.body.name,
+    });
+    return res.status(204).json();
+  } catch (error) {
+    return res.status(500).send(error);
+  }
+});
+
+exports.app = functions.https.onRequest(app);
